1f128cfcb8368b216f198850210bcc29cd63877c
hanhaoxian <haoxianhan@gmail.com>
2015-10-22 00:10:05 +0800

    Add common sort
    for gen tree

---
+++ huffman/Frequency.hs
@@ -0,0 +1,36 @@
+module Frequency (frequency)
+
+mergeSort merge xs
+  | length xs < 2     = xs
+  | otherwise
+    = merge (mergeSort merge first)
+            (mergeSort merge second)
+      where
+        first   = take half xs
+        second  = drop half xs
+        half    = div (length xs) 2
+
+
+alphaMerge :: [(Char, Int)] -> [(Char, Int)] -> [(Char, Int)]
+alphaMerge xs [] = xs
+alphaMerge [] ys = ys
+alphaMerge xa@((p, n):xs) ya@((q, m):ys)
+  | (p == q)  = (p, n+m):alphaMerge xs ys
+  | (p < q)   = (p, n):alphaMerge xs ya
+  | otherwise = (q, m):alphaMerge xa ys
+
+
+freqMerge :: [(Char, Int)] -> [(Char, Int)] -> [(Char, Int)]
+freqMerge xs [] = xs
+freqMerge [] ys = ys
+freqMerge xa@((p, n):xs) ya@((q, m):ys)
+  | (n < m || (n==m && p<q))
+      = (p, n):freqMerge xs ys
+  | otherwise
+      = (q, m):freqMerge xa ys
+
+
+frequency :: [Char] -> [(Char, Int)]
+frequency =
+  mergeSort freqMerge . mergeSort alphaMerge . map start
+    where start ch = (ch, 1)
